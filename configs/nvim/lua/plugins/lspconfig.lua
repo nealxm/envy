4f19e758ffa49cfcdbf680024335d3ec9a66cc5b
nealxm <88364802+nealxm@users.noreply.github.com>
2025-06-15 12:55:10 -0500

    editor bump

--- configs/nvim/lua/plugins/lspconfig.lua
+++ configs/nvim/lua/plugins/lspconfig.lua
@@ -93,10 +93,6 @@ return {
         }
         local ensure_installed = vim.tbl_keys(servers or {})
         vim.list_extend(ensure_installed, {
-            "lua_ls",
-            "gopls",
-            "ruff",
-            "zls",
         })
         require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
     build = ":TSUpdate",
     branch = "main",
     opts = {
-        ensure_installed = { "lua", "markdown", "toml", "vimdoc", "zig", "go" },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true }
